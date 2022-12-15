01d993d4c665460bb7be34a1fae73e815d86608d
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-15 08:34:15 +0300

    firs upload

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
 
-if (extension_loaded("pgsql")) {
-    echo "PostgreSQL arabirimi kurulu.";
-} else {
-    echo "PostgreSQL arabirimi kurulu değil.";
+$ayarlar = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_CASE => PDO::CASE_NATURAL,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+$baglanti_cumlesi = sprintf(
+    "pgsql:host=%s port=%s dbname=%s user=%s password=%s",
+    "localhost",
+    "5432",
+    "postoffice",
+    "postgres",
+    "00204063f4b4!NN"
+);
+
+try {
+
+    $baglanti = new PDO($baglanti_cumlesi, null, null, $ayarlar);
+
+} catch (PDOException $ex) {
+    die($ex->getMessage());
 }
 
-
-echo php_ini_loaded_file();
+$baglanti = null;
 
 ?>
