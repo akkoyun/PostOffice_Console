273ba0eb3300c3db297d811064aa5156b67ff5a0
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-12-15 08:44:37 +0300

    firs upload

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php
 
 $ayarlar = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_CASE => PDO::CASE_NATURAL,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_CASE => PDO::CASE_NATURAL,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
+
 $baglanti_cumlesi = sprintf(
-    "pgsql:host=%s port=%s dbname=%s user=%s password=%s",
-    "localhost",
-    "5432",
-    "PostOffice",
-    "postgres",
-    "00204063f4b4!NN"
+	"pgsql:host=%s port=%s dbname=%s user=%s password=%s",
+	"localhost",
+	"5432",
+	"PostOffice",
+	"postgres",
+	"00204063f4b4!NN"
 );
 
 try {
-
-    $baglanti = new PDO($baglanti_cumlesi, null, null, $ayarlar);
-
+	$baglanti = new PDO($baglanti_cumlesi, null, null, $ayarlar);
 } catch (PDOException $ex) {
-    die($ex->getMessage());
+	die($ex->getMessage());
 }
 
 //$baglanti = null;
 
+echo "Device List";
 
 $sorgu = $baglanti->query("SELECT * FROM Module");
 $cikti = $sorgu->fetch(PDO::FETCH_ASSOC);
